b5158a7a79b66a432c00eb5231047e21ee1c4927
rene <rene>
2024-12-03 11:06:09 +0100

    daythree part 1

---
+++ daythree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using static System.Console;
+
+var input = File.ReadAllText("input.txt");
+
+var pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+
+var matches = Regex.Matches(input, pattern);
+
+int sum = 0;
+
+for(int i = 0; i < matches.Count; i++)
+{
+    var match = matches[i];
+    var x = int.Parse(match.Groups[1].Value);
+    var y = int.Parse(match.Groups[2].Value);
+    WriteLine("Found: " + match.Value);
+    var result = x * y;
+    WriteLine("Result: " + result);
+    sum += result;
+}
+WriteLine("===");
+WriteLine("Sum: " + sum);
