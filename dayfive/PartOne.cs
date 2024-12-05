6f6f052e94260a8ccd4bd37f9d90616004f303df
rene <rene>
2024-12-05 16:20:51 +0100

    dayfive part 1

---
+++ dayfive/PartOne.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+
+class PartOne
+{
+    public void CalculateAndPrint(List<Rule> rules, List<Update> updates)
+    {
+        WriteLine("====== PART ONE ======");
+
+        var sum = 0;
+
+        foreach (var update in updates)
+        {
+            if (UpdateIsValid(update, rules))
+            {
+                sum += GetMiddleValueInUpdate(update);
+            }
+        }
+        WriteLine($"Sum: {sum}");
+        WriteLine("======================");
+    }
+
+    bool UpdateIsValid(Update update, List<Rule> rules)
+    { 
+        var sequence = update.sequence;
+        foreach (var rule in rules)
+        {
+            if(sequence.Contains(rule.left) && sequence.Contains(rule.right))
+            {
+                var valid = CheckRule(rule, sequence);
+                if (!valid) return false;
+            }
+        }
+        return true;
+    }
+
+    bool CheckRule(Rule rule, List<int> sequence)
+    {
+        var leftIndex = sequence.FindIndex(number => number == rule.left);
+        var rightIndex = sequence.FindIndex(number => number == rule.right);
+        return leftIndex < rightIndex;
+    }
+
+
+    int GetMiddleValueInUpdate(Update update)
+    {
+        return update.sequence[update.sequence.Count / 2];
+    }
+}
---
+++ dayfive/Program.cs
@@ -0,0 +1,40 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+List<Rule> rules = [];
+List<Update> updates = [];
+var flip = false;
+foreach (var line in input)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        flip = true;
+        continue;
+    }
+
+    if (!flip)
+        rules.Add(CreateRule(line));
+    else
+    {
+        updates.Add(CreateUpdate(line));
+    }
+
+}
+
+new PartOne().CalculateAndPrint(rules, updates);
+
+Rule CreateRule(string line)
+{
+    var splits = line.Split('|', 2);
+    var left = int.Parse(splits[0]);
+    var right = int.Parse(splits[1]);
+    return new Rule(left, right);
+}
+
+Update CreateUpdate(string line)
+{
+    var sequence = line.Split(',').Select(int.Parse).ToList();
+    return new Update(sequence);
+}
+
+record Rule(int left, int right);
+record Update(List<int> sequence);
